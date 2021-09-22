3ef37640a2652a75377492a687725a4395ab970e
Emile Steenkamp <emile@steenkamps.org>
2021-09-23 00:43:12 +0200

    finished workflow implementation

--- src/main/kotlin/Workflow.kt
+++ src/main/kotlin/Workflow.kt
@@ -2,54 +2,128 @@ import kotlin.reflect.KFunction
 import kotlin.reflect.KParameter
 import kotlin.reflect.KProperty
 
-//interface WorkflowInterface
-//fun <T : Workflow> T.execute(): T {
-//    val kClass = this.javaClass.kotlin
-//    val workflowEnvironmentMap = mutableMapOf<String, Any?>()
-//
-//    val properties = kClass.members.filterIsInstance<KProperty<*>>()
-//    val functions = kClass.members.filterIsInstance<KFunction<*>>()
-//
-//    for (property in properties) {
-//        workflowEnvironmentMap[property.name.toString()] = property.getter.call(this)
-//    }
-//
-//    for (function in functions) {
-//        callFunction(function, workflowEnvironmentMap)
-//    }
-//
-//    return this;
-//}
-
 abstract class Workflow {
-    fun execute() {
-        val kClass = this.javaClass.kotlin
-        val workflowEnvironmentMap = mutableMapOf<String, Any?>()
+    private val kProperties: List<KProperty<*>> = this.javaClass.kotlin.members.filterIsInstance<KProperty<*>>()
+    private val kFunctions: List<KFunction<*>> = this.javaClass.kotlin.members.filterIsInstance<KFunction<*>>()
+    val environment = mutableMapOf<String, Any?>()
 
-        val properties = kClass.members.filterIsInstance<KProperty<*>>()
-        val functions = kClass.members.filterIsInstance<KFunction<*>>()
+    abstract fun getResult(): Any
 
-        for (property in properties) {
-            workflowEnvironmentMap[property.name.toString()] = property.getter.call(this)
+    fun execute(): Workflow {
+        this.generateEnvironment()
+        val start = this.kFunctions.find { kFunction -> kFunction.annotations.filterIsInstance<Start>().isNotEmpty() }
+
+        if (start == null) {
+            throw Exception("No Start found.")
+        } else {
+            callFunction(start)
         }
 
-        for (function in functions) {
-            this.callFunction(function, workflowEnvironmentMap)
+        return this
+    }
+
+    private fun generateEnvironment() {
+        for (kProperty in this.kProperties) {
+            this.environment[kProperty.name] = kProperty.getter.call(this)
         }
     }
 
-    private fun callFunction(function: KFunction<*>, workflowEnvironmentMap: Map<String, Any?>) {
-        val args = mutableMapOf<KParameter, Any?>()
+    private fun callFunction(kFunction: KFunction<*>, ) {
+        val arguments = this.generateArguments(kFunction)
+        val resultAnnotation = kFunction.annotations.filterIsInstance<Result>().firstOrNull()
 
-        var parameters = function.parameters
-        args[parameters.first()] = this
+        if (resultAnnotation == null) {
+            kFunction.callBy(arguments)
+        } else {
+            this.environment[resultAnnotation.resultString] = kFunction.callBy(arguments)
+        }
+
+        transitionTemporary(kFunction)
+    }
+
+    private fun transition(kFunction: KFunction<*>) {
+        val transitionAnnotations = kFunction.annotations.filterIsInstance<Transition>()
+
+        for (transitionAnnotation in transitionAnnotations) {
+            if (transitionAnnotation.condition == "") {
+                if (transitionAnnotation.transitionString == "End") {
+                    return
+                } else {
+                    callTransitionFunction(transitionAnnotation.transitionString)
+                }
+            } else if (this.isConditionTrue(transitionAnnotation.condition)) {
+                if (transitionAnnotation.transitionString == "End") {
+                    return
+                } else {
+                    callTransitionFunction(transitionAnnotation.transitionString)
+                }
+            }
+        }
+
+        throw Exception("No valid transition")
+    }
+
+    private fun transitionTemporary(kFunction: KFunction<*>) {
+        val transitionAnnotation = kFunction.annotations.filterIsInstance<TransitionTemporary>().first()
+
+        for (transition in transitionAnnotation.transitions) {
+            val transitionSplit = transition.split("->")
+            val condition = transitionSplit[0]
+            val transitionString = transitionSplit[1]
+
+            if (condition == "") {
+                if (transitionString == "END") {
+                    return
+                } else {
+                    callTransitionFunction(transitionString)
+                }
+            } else if (this.isConditionTrue(condition)) {
+                if (transitionString == "END") {
+                    return
+                } else {
+                    callTransitionFunction(transitionString)
+                }
+            }
+        }
+    }
+
+    private fun callTransitionFunction(transitionString: String) {
+        val next = kFunctions.first { kFunction -> kFunction.name == (transitionString) }
+
+        callFunction(next)
+    }
+
+    private fun isConditionTrue(condition: String): Boolean {
+        return if (this.shouldNegate(condition)) {
+            val conditional = this.environment[condition.drop(1)]
+
+            conditional == false
+        } else {
+            val conditional = this.environment[condition]
+
+            conditional == true
+        }
+    }
+
+    private fun shouldNegate(condition: String): Boolean
+    {
+        return condition.first() == '!'
+    }
+
+    private fun generateArguments(
+        kFunction: KFunction<*>,
+    ): MutableMap<KParameter, Any?> {
+        val arguments = mutableMapOf<KParameter, Any?>()
+
+        var parameters = kFunction.parameters
+        arguments[parameters.first()] = this
         parameters = parameters.drop(1)
 
         for (parameter in parameters) {
-            args[parameter] = workflowEnvironmentMap[parameter.name.toString()]
+            arguments[parameter] = this.environment[parameter.name.toString()]
         }
 
-        function.callBy(args)
+        return arguments
     }
 }
 
@@ -63,4 +137,7 @@ annotation class Result(val resultString: String)
 
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class Transition(val transitionString: String, val condition: String)
+@Repeatable
+annotation class Transition(val condition: String, val transitionString: String)
+
+annotation class TransitionTemporary(val transitions: Array<String>)
--- src/test/kotlin/WorkflowTest.kt
+++ src/test/kotlin/WorkflowTest.kt
@@ -3,36 +3,60 @@ import org.junit.Test
 internal class WorkflowTest {
     @Test
     fun testWorkflow() {
-        val user = WorkflowUserCreate("Emile", 23, "ZA").execute()
+        val user = CreateUser("Emile", 22, "ZA").execute().getResult()
+
+        println(user.toString())
     }
 }
 
-class WorkflowUserCreate(val name: String, val age: Number, val country: String) : Workflow() {
+class CreateUser(val name: String, val age: Number, val country: String) : Workflow() {
     @Start
-    @Transition("isSpecialPerson", "")
-    fun assertUserUnique(name: String) {
+    @Result("isUserUnique")
+    @TransitionTemporary([
+            "->isSpecialPerson"
+    ])
+    fun assertUserUnique(name: String): Boolean {
         if (name == "Not Unique") {
             throw Exception();
         } else {
-            // The user is unique.
+            return true
         }
     }
 
-    @Transition("createTag", "")
+    @Result("isSpecialPerson")
+    @TransitionTemporary([
+        "isSpecialPerson->createTag",
+        "!isSpecialPerson->createUser"
+    ])
     fun isSpecialPerson(name: String, age: Number): Boolean {
         return name == "Emile" && age == 23;
     }
 
-    @Transition("createUser", "")
     @Result("tag")
+    @TransitionTemporary([
+        "->createUser",
+    ])
     fun createTag(): String {
         return "special"
     }
 
     @Result("user")
+    @TransitionTemporary([
+        "->END"
+    ])
     fun createUser(name: String, age: Number, country: String, tag: String?): User {
         return User(name, age, country, tag);
     }
+
+    override fun getResult(): User {
+        val result = this.environment["user"]
+
+        if (result is User) {
+            return result
+        } else {
+            throw Exception("Unexpected result")
+        }
+    }
 }
 
-class User(name: String, age: Number, country: String, tag: String?)
+class User(val name: String, val age: Number, val country: String, val tag: String?)
