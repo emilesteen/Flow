6c01ece9c4b28b536c43a8e4e3535321e5bd36a6
Emile Steenkamp <emile@steenkamps.org>
2021-12-27 16:05:56 +0100

    update gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    java
     kotlin("jvm") version "1.6.0"
     `maven-publish`
 }
@@ -23,4 +24,16 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.emilesteen"
+            artifactId = "flow-tree"
+            version = "0.0.0"
+
+            from(components["java"])
+        }
+    }
 }
